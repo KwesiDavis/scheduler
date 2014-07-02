8ff8e8441830d78e7ce58f4fb57fe6fc351cf179
Kwesi Davis <kwesi.a.davis@gmail.com>
2014-07-02 16:17:25 -0700

    Run a network with three components.

--- scheduler.py
+++ scheduler.py
@@ -10,24 +10,36 @@ def add(a, b, sum):
 # --inputs for 'add'
 net_in_a, add_in_a = Pipe()
 net_in_b, add_in_b = Pipe()
-# --output for 'add'
-add_out_sum, net_out_sum = Pipe()
+# --inputs for 'add2'
+net_in_c, add2_in_a = Pipe()
+net_in_d, add2_in_b = Pipe()
+# --inputs for 'add3'
+add_out_sum, add3_in_a = Pipe()
+add2_out_sum, add3_in_b = Pipe()
+# --output for 'add3'
+add3_out_sum, net_out_sum = Pipe()
 
-# create process instance
-comp_add = Process(target=add, args=(add_in_a, add_in_b, add_out_sum))
-
-# stimulate network with iip(s)
-net_in_a.send(3)
-net_in_b.send(5)
-for conn in [net_in_a, net_in_b]:
-    conn.close()
+# create process instance(s)
+comp_add  = Process(target=add, args=(add_in_a, add_in_b, add_out_sum))
+comp_add2 = Process(target=add, args=(add2_in_a, add2_in_b, add2_out_sum))
+comp_add3 = Process(target=add, args=(add3_in_a, add3_in_b, add3_out_sum))
 
 # run network
-comp_add.start()
+for comp in [comp_add, comp_add2, comp_add3]:
+    comp.start()
+
+# stimulate network with iip(s)
+net_in_a.send(1)
+net_in_b.send(2)
+net_in_c.send(3)
+net_in_d.send(4)
+for conn in [net_in_a, net_in_b, net_in_c, net_in_d]:
+    conn.close()
 
 # wait for results
 print net_out_sum.recv()
 net_out_sum.close()
 
-# terminate child before parent exits
-comp_add.join()
+# terminate children before parent exits
+for comp in [comp_add, comp_add2, comp_add3]:
+    comp.join()
