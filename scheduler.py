990e86194251e4ba9cdcd8ae5b793dc1df94d86a
Kwesi Davis <kwesi.a.davis@gmail.com>
2014-07-03 23:01:31 -0700

    Support reading a graph from a JSON file and building a network programmatically.

--- scheduler.py
+++ scheduler.py
@@ -1,45 +1,177 @@
+import json
 from multiprocessing import Process, Pipe
 
-# define component
-def add(a, b, sum):
+def externalConnIter(graph, inports=True):
+    '''
+    Given a graph iterate over the connections that touch an external port.
+    
+    Parameters:
+        graph - A graph of data relationships between the ports of components 
+        inports - When 'True', the default, include connections that involve 
+                  both in-ports and out-ports. Otherwise, ignore in-ports.
+    Returns:
+        Yields a tuple of the form: (srcProcessName, srcPortName, 
+        tgtProcessName, tgtPortName, None)     
+    '''
+    type2direction = {'outports' : ('tgtProcessName', 'tgtPortName',  'srcProcessName', 'srcPortName')}
+    if inports:
+       type2direction.update({'inports' : ('srcProcessName', 'srcPortName', 'tgtProcessName', 'tgtPortName')})
+    for portType, (mainProcessName, mainPortName, internalProcessName, internalPortName) in type2direction.items():
+        retval = {}
+        for externalPortName in graph[portType]:
+            retval[mainProcessName]     = '__main__'
+            retval[mainPortName]        = externalPortName
+            retval[internalProcessName] = graph[portType][externalPortName]['process']
+            retval[internalPortName]    = graph[portType][externalPortName]['port']
+            yield retval['srcProcessName'], retval['srcPortName'], retval['tgtProcessName'], retval['tgtPortName'], None
+    
+def internalConnIter(graph, iips=True):
+    '''
+    Given a graph iterate over the connections that *do not* touch an external port.
+    
+    Parameters:
+        graph - A graph of data relationships between the ports of components 
+        iips - When 'True', the default, include connections represented as 
+               IIPs embedded in the given graph. Otherwise, ignore these IIPs.   
+    Returns:
+        Yields a tuple of the form: (srcProcessName, srcPortName, 
+        tgtProcessName, tgtPortName, data)     
+    '''
+    iipCount = 0
+    for connection in graph['connections']:
+        data = None
+        try:
+            srcProcessName = connection['src']['process']
+            srcPortName    = connection['src']['port']
+        except KeyError:
+            if not iips: continue
+            srcProcessName = '__main__'
+            srcPortName    = '__%s__' % iipCount
+            iipCount      += 1
+            data           = connection['data']
+        tgtProcessName = connection['tgt']['process']
+        tgtPortName    = connection['tgt']['port']
+        yield srcProcessName, srcPortName, tgtProcessName, tgtPortName, data
+
+def graph2network(graph, library, asIs=True):
+    '''
+    Given a graph and a component library generate a sub-network of Python
+    multiprocessing Process objects wired to together with by Pipe objects.
+    
+    Parameters:
+        graph - A graph of data relationships between the ports of components
+        library - A dictionary mapping component names to a function object 
+                  that represents the component's business logic.
+        asIs - When 'True', the default, the IIPs embedded in the given graph
+               trump any connections coming from in-ports.   
+    Returns:
+        A tuple of the form: (iips, mainProcesInterface, subProcesses)     
+    '''
+    # parse connections
+    iips   = []
+    kwargs = {}
+    for iter in [externalConnIter(graph, inports=(not asIs)), internalConnIter(graph, iips=asIs)]:
+        for srcProcessName, srcPortName, tgtProcessName, tgtPortName, data in iter:
+            connSrc, connTgt = Pipe()         
+            srcPortNameStr = str(srcPortName) # unicode-to-str
+            kwargs.setdefault(srcProcessName, {})[srcPortNameStr] = connSrc
+            tgtPortNameStr = str(tgtPortName) # unicode-to-str
+            kwargs.setdefault(tgtProcessName, {})[tgtPortNameStr] = connTgt
+            if data:
+                iips.append((connSrc, data))
+    # parse processes
+    subProcesses = []
+    for processName in graph['processes'].keys():
+        fxn = library[graph['processes'][processName]['component']]
+        subProcesses.append( Process(target=fxn, kwargs=kwargs[processName]) )
+    return iips, kwargs['__main__'], subProcesses
+            
+def startNetwork(processes):
+    '''
+    Run the sub-network of process.
+    
+    Parameters:
+        processes - A complete list of processes in the sub-network
+    '''
+    for process in processes:
+        process.start()
+
+def runNetwork(iips):
+    '''
+    Stimulate sub-network with initial information packets (or IIPs).
+    
+    Parameters:
+        iips - A list of tuples of the form: (connection, data)
+    '''
+    for conn, data in iips:
+        conn.send(data)
+        conn.close()
+        
+def printOutPorts(interface, outportNames):
+    '''
+    Display the results of sub-network.
+    
+    Parameters:
+        interface - A dictionary mapping external port names to connections.
+        outportNames - 
+    '''
+    for portName, conn in interface.items():
+        if portName in outportNames:
+            # wait for results
+            print "__main__.%s = %s" % (portName, conn.recv())
+            conn.close()
+
+def stopNetwork(processes):
+    '''
+    Shutdown the sub-network of process.
+    
+    Parameters:
+        processes - A complete list of processes in the sub-network
+    '''
+    for process in processes:
+        # Wait of all children to terminate themselves so we can exit the main
+        # process without leaving orphaned processes behind.    
+        process.join()
+
+def json2graph(path):
+    '''
+    Load the given JSON graph file.
+    
+    Parameters:
+        path - A path to a JSON file.
+    Returns:
+        A dictionary representing graph of data relationships between the 
+        ports of components.
+    '''
+    f = open(path, "r")
+    return json.loads(f.read())
+
+def add(a=None, b=None, sum=None):
+    '''
+    Logic for a simple 'Add' component.
+    
+    Parameters:
+        a - A connection to receive the left addend.
+        b - A connection to receive the right addend.
+        sum - A connection to send the result of a+b.        
+    '''
     sum.send(a.recv()+b.recv())
     for conn in [a, b, sum]:
         conn.close()
 
-# wire-up connection(s)
-# --inputs for 'add'
-net_in_a, add_in_a = Pipe()
-net_in_b, add_in_b = Pipe()
-# --inputs for 'add2'
-net_in_c, add2_in_a = Pipe()
-net_in_d, add2_in_b = Pipe()
-# --inputs for 'add3'
-add_out_sum, add3_in_a = Pipe()
-add2_out_sum, add3_in_b = Pipe()
-# --output for 'add3'
-add3_out_sum, net_out_sum = Pipe()
-
-# create process instance(s)
-comp_add  = Process(target=add, args=(add_in_a, add_in_b, add_out_sum))
-comp_add2 = Process(target=add, args=(add2_in_a, add2_in_b, add2_out_sum))
-comp_add3 = Process(target=add, args=(add3_in_a, add3_in_b, add3_out_sum))
-
-# run network
-for comp in [comp_add, comp_add2, comp_add3]:
-    comp.start()
-
-# stimulate network with iip(s)
-net_in_a.send(1)
-net_in_b.send(2)
-net_in_c.send(3)
-net_in_d.send(4)
-for conn in [net_in_a, net_in_b, net_in_c, net_in_d]:
-    conn.close()
-
-# wait for results
-print net_out_sum.recv()
-net_out_sum.close()
-
-# terminate children before parent exits
-for comp in [comp_add, comp_add2, comp_add3]:
-    comp.join()
+if __name__ == '__main__':
+    # Create a library of supported components
+    library = {'Add':add}  
+    # Load a graph from disk
+    path    = './graphs/add_tree.json'
+    graph   = json2graph(path)
+    # Build a network from the graph
+    network = graph2network(graph, library)
+    iips, interface, processes = network
+    # Run the network
+    startNetwork(processes)
+    runNetwork(iips)
+    # Display the network's output
+    printOutPorts(interface, graph['outports'])
+    # Tear down the network
+    stopNetwork(processes)
