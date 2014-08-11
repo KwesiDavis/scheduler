093ebee41b92c9f41ca2a99315f15e2de4d89527
Kwesi Davis <kwesi.a.davis@gmail.com>
2014-08-11 16:35:42 -0700

    simplified fix for Pipe() file descriptor leak

--- scheduler/util/plumber.py
+++ scheduler/util/plumber.py
@@ -1,5 +1,5 @@
 import logging, os
-from multiprocessing import Pipe, Process
+from multiprocessing import Process
 from threading import Thread
 
 '''
@@ -55,44 +55,143 @@ def compareWorkers(a,b):
         retval =  0
     return retval
     
-def start(workers, leak):
+def start(workers, leak, processName):
     '''
-    Closing unused file descriptors held by threads, closes them in the parent 
-    process also. The parent process passes a copy of its file descriptors to 
-    its spawned child processes. So this method makes sure all Threads start 
-    after all Processes, so processes don't inherit closed file descriptors.
-    
+    Starts all workers, but also makes sure that all Process workers start 
+    *before* any Thread workers.      
+    Note: Child worker Threads and Processes inherit open connections (or file
+          descriptors) from the main parent process at start-time.  This method 
+          starts child Threads (and child Processes) from the main parent 
+          thread.  After a Thread starts, it will close all connections not 
+          used by itself (or any other sibling thread).  When a Thread closes a
+          connection, it also closes it in all sibling Threads and in the main 
+          parent thread.  Thus, any Thread that starts, will close all 
+          connections (in the main parent thread) used by all Processes.  So,
+          any Process that starts, after any Thread, will inherit closed 
+          connections that it expects to be open for use.  This is bad.
+          
     Parameters:
         workers - A list of workers (Process or Thread objects)
-        leakyPipes - A list of Pipes created in the parent process; which have
-                     leaked their file descriptors into child workers. 
+        leak - A list of all connections (or file descriptors) in-use by the 
+               network of processes.        
+        processName - The parent (or composite) process responsible for managing 
+                      its network sub-processes. 
     '''
     for worker in sorted(workers, cmp=compareWorkers):
         worker.start()
-    # Handle un-used file descriptors held by the parent process.
-    processName = '<none>'
-    connInfos, sharing = leak
-    closeByProcess(connInfos, processName, sharing=sharing)
+    # Closed un-used file descriptors held by the given process.
+    closeByProcess(leak, processName)
 
-def connectionInfo( conn, process, port, parent=None ):
+def connectionInfo(conn, process, port, parent=None):
     '''
+    One end of a Pipe (or a Connection object) and metadata describing who is 
+    using the connection object.
+    Note: This metadata is needed to handle the Pipe file descriptor leak.
+    
+    Parameters:
+        conn - a Connection object returned from Pipe()
+        process - the process using the connection object 
+        port - the port, on the given process, being connected to
+        parent - the composite-process that contains the given process
+    Returns:
+        A dictionary representing one end of a Pipe. 
     '''
     return { 'connection' : conn,
              'process'    : process,
              'port'       : port,
              'parent'     : parent }
 
-def closeByProcess(connInfos, process, sharing=[]):
-    if process in sharing:
+def closeByProcess(leak, processName):
+    '''
+    Close all connections (in the given list of leaked connections) that will
+    not be used by the given process.
+    Note: If the given process is a worker thread, the connections it "uses" 
+          includes the connections being used by all other threads.   
+     
+    Parameters:
+        leak - A list of all connections (or file descriptors) in-use by the 
+               network of processes.
+        processName - A process using connections on the network. 
+    '''
+    if processName in leak['threads']:
         # Treat all aliases as one since they share the same process id
-        processNames = sharing
+        processNames = leak['threads']
     else:
-        processNames = [ process ]
+        processNames = [ processName ]
         
     pid = os.getpid()
-    for connInfo in connInfos['inports'] + connInfos['outports']:
-        logging.debug('{prefix}: [{pid}] On init, process "{proc}" opened "{pipeProc}.{pipePort}".'.format(prefix='LEAK', pid=pid, pipeProc=connInfo['process'], pipePort=connInfo['port'], proc=process))     
+    for connInfo in leak['connections']['inports'] + leak['connections']['outports']:
+        logging.debug('LEAK: [{pid}] On init, process "{proc}" opened "{pipeProc}.{pipePort}".'.format(pid=pid, pipeProc=connInfo['process'], pipePort=connInfo['port'], proc=processName))
+        # MAINT: Take connInfo['parent'] into account     
         if connInfo['process'] in processNames:
             continue
-        logging.debug('LEAK: [{pid}] On init, process "{proc}" closed "{pipeProc}.{pipePort}".'.format(pid=pid, pipeProc=connInfo['process'], pipePort=connInfo['port'], proc=process))
+        logging.debug('LEAK: [{pid}] On init, process "{proc}" closed "{pipeProc}.{pipePort}".'.format(pid=pid, pipeProc=connInfo['process'], pipePort=connInfo['port'], proc=processName))
         connInfo['connection'].close()
+
+def newLeak(inports=[], outports=[], threads=set([])):
+    '''
+    Create an object that knows about all of the leaked Pipe connections and 
+    the names of all the Thread processes.
+    
+    Parameters:
+        inports - A list of in-port connections (and associated metadata)
+                  in-use by a network of processes. 
+        outports - A list of out-port connections (and associated metadata) 
+                  in-use by a network of processes. 
+        threads - the names of workers that share a PID
+    Returns:
+        A dictionary representing A list of all connections (and associated 
+        metadata) in-use by a network of processes. one end of a Pipe. 
+    '''
+    return { 'connections' : { 'inports'  : inports,
+                               'outports' : outports },
+             'threads'     : threads }
+
+def getLeakByProcess(leak, processName):
+    '''
+    Create an object that knows about all of the leaked Pipe connections
+    associated with a given process.
+    
+    Parameters:
+        leak - A list of all connections (or file descriptors) in-use by the 
+               network of processes.
+        processName -  A process using connections on the network. 
+    Returns:
+        A dictionary representing A list of all connections (and associated 
+        metadata) in-use by a network of processes. one end of a Pipe. 
+    '''    
+    # MAINT: This will fail for threaded subnets so we should pass all process name aliases
+    ins     = [ connInfo for connInfo in leak['connections']['inports']  if connInfo['process'] == processName]
+    outs    = [ connInfo for connInfo in leak['connections']['outports'] if connInfo['process'] == processName]
+    threads = set([processName])
+    return newLeak(inports=ins, outports=outs, threads=threads)
+
+def append(leak, parentProcessName, connection, processName, portName, isThread, inport=True):
+    '''
+    Add a new leaked connection object (and associated metadata) to an existing 
+    list of Pipe connections used by a network.
+    
+    Parameters:
+        leak - An existing list of all connections (or file descriptors) 
+               in-use by the network of processes.
+        connection - A connection (or one end of a Pipe) 
+        processName - The process, on the network, that will use the given 
+                      connection.
+        portName - The port, on the given process, that will use the given 
+                   connection.
+        isThread - Is 'True', when the given process shares its PID with some 
+                   other worker in the network. Is 'False' otherwise.
+        inport - Is 'True', when the given port is and in-port. Is 'False' 
+                 otherwise. 
+    '''    
+    portType = {True:'inports', False:'outports'}
+    # Get namespace for the given process 
+    namespace = parentProcessName
+    isRoot = parentProcessName == processName
+    if isRoot:
+        namespace = None
+    # Keep a list of all in-ports and out-ports
+    leak['connections'].setdefault(portType[inport], []).append( connectionInfo(connection, processName, portName, parent=namespace) )
+    # Keep track of workers that share a PID (and thus share file descriptors)
+    if isThread:
+        leak['threads'].add(processName)
